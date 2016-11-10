8c7337686e2906e28d79c1065f1fcb27c31d5c76
foskasse <andre.duarte.it@gmail.com>
2016-11-10 17:40:48 +0000

    df

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+include_once("simple_html_dom.php");
+
+// set target url to crawl
+$url = "https://www.einforma.pt/novas-empresas"; // change this
+
+// open the web page
+$html = new simple_html_dom();
+$html->load_file($url);
+
+// array to store scraped links
+$links = array();
+
+// crawl the webpage for links
+foreach($html->find("a") as $link){
+    array_push($links, $link->href);
+}
+
+// remove duplicates from the links array
+$links = array_unique($links);
+
+// set output headers to download file
+//header("Content-Type: text/csv; charset=utf-8");
+//header("Content-Disposition: attachment; filename=links.csv");
+
+// set file handler to output stream
+$output = fopen("php://output", "w");
+// output the scraped links
+
+//$rest = substr("$links");
+function get_string_between($string, $start, $end){
+    $string = ' ' . $string;
+    $ini = strpos($string, $start);
+    if ($ini == 0) return '';
+    $ini += strlen($start);
+    $len = strpos($string, $end, $ini) - $ini;
+    return substr($string, $ini, $len);
+}
+
+
+$a json_encode($links);
+
+$parsed = get_string_between($a, '[tag]', '[/tag]');
+
+//echo $parsed; // (result = dog)
+//print_r($links);
+// fputcsv($output, $links, "\n");
+?>
